64ba1eaae21e496fa7edd79924d3c81485bdecf9
vasil <v.tsigov@iag.bg>
2019-06-24 13:32:05 +0300

    Edit config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php 
 
 return  [
-    'servername' => '172.16.4.34',
-    'username' => 'cotaivo',
-    'password' => 'taniami',
-    'dbname' => 'iag112new'
+    'servername' => '',
+    'username' => '',
+    'password' => '',
+    'dbname' => ''
 ];
